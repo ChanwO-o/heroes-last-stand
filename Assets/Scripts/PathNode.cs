99a48ce07e703edb9c20f58b19029d6717d9a197
Anthony Reksoatmodjo <areksoat@uci.edu>
2020-10-24 19:49:29 -0700

    Brought code to the main codebase

---
+++ Assets/Scripts/PathNode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode : MonoBehaviour
+{
+    public List<PathNode> next = null;
+    public bool end;
+
+    // If we have multiple nodes, return random
+    public PathNode getNextNode(){
+        if(next.Count > 1){
+            int i = Random.Range(0, next.Count);
+            return next[i];
+        }
+        else
+        {
+            return next[0];
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+#if UNITY_EDITOR
+        if (next[0] != null)
+        {
+            for(int i = 0; i < next.Count; i++){
+                // Draws a blue line from this transform to the target
+                Gizmos.color = Color.blue;
+                Gizmos.DrawLine(transform.position, next[i].transform.position);
+            }
+
+        }
+    }
+#endif
+}
---
+++ Assets/Scripts/PathObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathObject : MonoBehaviour
+{
+    // Hold an array of transforms aka path nodes
+    [SerializeField]
+    public List<Transform> nodes = new List<Transform>();    
+}
