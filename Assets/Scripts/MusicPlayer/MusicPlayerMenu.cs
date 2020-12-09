22c853b3873904863fa151dac30dbeb000448a17
Jona Huang <jonnyboy.345345@gmail.com>
2020-12-08 22:16:34 -0800

    Integrated looping music into the scenes

---
+++ Assets/Scripts/MusicPlayer/MusicPlayerMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerMenu : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject musicGO = GameObject.FindGameObjectWithTag("Music");
+        musicGO.GetComponent<MusicPlayer>().PlayMusic(0);
+    }
+}
