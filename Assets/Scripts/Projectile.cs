be15cec132b8b44dc5d5a54e8f963c2265e39539
Anthony Reksoatmodjo <areksoat@uci.edu>
2020-12-09 10:12:10 -0800

    rem debug

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -16,11 +16,9 @@ public class Projectile : MonoBehaviour
     // This callback is only triggered if one of the colliders also
     // has a Rigidbody2d, set to Dynamic mode (not Kinematic)
     private void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("WE HIT SOMETHING");
         // We only want to do damage to enemies.
         // No friendly fire!
         if(other.gameObject.tag == "Enemy"){
-            Debug.Log("We hit an ENEMY");
             other.gameObject.GetComponent<PathFollower>().doDamage(damage);
             Destroy(this.gameObject);
         }
